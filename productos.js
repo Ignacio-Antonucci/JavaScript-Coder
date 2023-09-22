4c1beebeb919877c17bb2bc864ceb7a5dbdd5979
Ignacio Antonucci <nacho_antonucci@hotmail.com.ar>
2023-09-21 23:10:38 -0300

    Se pasa todo a dom, y se expreimenta con JSON y LOCALStorage

---
+++ productos.js
@@ -0,0 +1,8 @@
+const productos =[
+    {id: 1, nombre: 'Campera naranja', precio:14000 ,imagen:'./imagenes/campera-naranja.jpeg'},
+    {id: 2, nombre: 'Campera azul', precio:14500 ,imagen:'./imagenes/campera-azul.jpg'},
+    {id: 3, nombre: 'Campera bordo', precio:15000,imagen:'./imagenes/campera-bordo.jpg'},
+    {id: 4, nombre: 'Campera roja', precio:18000,imagen:'./imagenes/campera-roja.jpg'},
+    {id: 5, nombre: 'Campera rosa', precio:13000,imagen:'./imagenes/campera-rosa.jpeg'},
+    {id: 6, nombre: 'Campera ', precio:17000,imagen:'./imagenes/campera-violeta.jpg'}
+]
